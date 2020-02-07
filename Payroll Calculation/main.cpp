e31f1f04120c99d6905492031a84642c30593faa
thestoneyboat <60492047+thestoneyboat@users.noreply.github.com>
2020-02-06 20:37:23 -0500

    Add files via upload
    Payroll Calculation

---
+++ Payroll Calculation/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//This program performs a payroll calculation
+
+int main()
+{
+   int a,b,c;
+    cout << "This program performs a payroll calculation" << endl;
+    cout << "Enter a Number." << endl;
+    cin >> a;
+    cout << "Enter another Number." << endl;
+    cin >> b;
+    c = a * b;
+    cout << c << endl;
+
+    int d,e,f,g;
+    cout << "This program calculates the product of three integers" << endl;
+    cout << "Enter a Number." << endl;
+    cin >> d;
+    cout << "Enter another Number." << endl;
+    cin >> e;
+    cout << "Enter a third Number" << endl;
+    cin >> f;
+    g = d * e * f;
+    cout << "The product is" << g << endl;
+    return 0;
+}
